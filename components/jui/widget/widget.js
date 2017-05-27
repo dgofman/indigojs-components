85cbcbd369f0533e9cf5932a6d27c9f1f6ace95f
David Gofman <dgofman@gmail.com>
2017-05-27 15:42:56 -0700

    added dropdown

--- components/jui/widget/widget.js
+++ components/jui/widget/widget.js
@@ -1426,4 +1426,90 @@ juiWidget.loaded = function($) {
 		}
 	};
 
+//https://github.com/jquery/jquery-ui/blob/master/ui/data.js
+	$.extend( $.expr.pseudos, {
+		data: $.expr.createPseudo ?
+			$.expr.createPseudo( function( dataName ) {
+				return function( elem ) {
+					return !!$.data( elem, dataName );
+				};
+			} ) :
+
+			// Support: jQuery <1.8
+			function( elem, i, match ) {
+				return !!$.data( elem, match[ 3 ] );
+			}
+	});
+
+//https://github.com/jquery/jquery-ui/blob/master/ui/tabbable.js
+	$.extend( $.expr.pseudos, {
+		tabbable: function( element ) {
+			var tabIndex = $.attr( element, "tabindex" ),
+				hasTabindex = tabIndex !== null;
+			return ( !hasTabindex || tabIndex >= 0 ) && $.ui.focusable( element, hasTabindex );
+		}
+	});
+
+//https://github.com/jquery/jquery-ui/blob/master/ui/focusable.js
+	$.ui.focusable = function( element, hasTabindex ) {
+		var map, mapName, img, focusableIfVisible, fieldset,
+			nodeName = element.nodeName.toLowerCase();
+
+		if ( "area" === nodeName ) {
+			map = element.parentNode;
+			mapName = map.name;
+			if ( !element.href || !mapName || map.nodeName.toLowerCase() !== "map" ) {
+				return false;
+			}
+			img = $( "img[usemap='#" + mapName + "']" );
+			return img.length > 0 && img.is( ":visible" );
+		}
+
+		if ( /^(input|select|textarea|button|object)$/.test( nodeName ) ) {
+			focusableIfVisible = !element.disabled;
+
+			if ( focusableIfVisible ) {
+
+				// Form controls within a disabled fieldset are disabled.
+				// However, controls within the fieldset's legend do not get disabled.
+				// Since controls generally aren't placed inside legends, we skip
+				// this portion of the check.
+				fieldset = $( element ).closest( "fieldset" )[ 0 ];
+				if ( fieldset ) {
+					focusableIfVisible = !fieldset.disabled;
+				}
+			}
+		} else if ( "a" === nodeName ) {
+			focusableIfVisible = element.href || hasTabindex;
+		} else {
+			focusableIfVisible = hasTabindex;
+		}
+
+		return focusableIfVisible && $( element ).is( ":visible" ) && visible( $( element ) );
+	};
+
+	// Support: IE 8 only
+	// IE 8 doesn't resolve inherit to visible/hidden for computed values
+	function visible( element ) {
+		var visibility = element.css( "visibility" );
+		while ( visibility === "inherit" ) {
+			element = element.parent();
+			visibility = element.css( "visibility" );
+		}
+		return visibility !== "hidden";
+	}
+
+	$.extend( $.expr.pseudos, {
+		focusable: function( element ) {
+			return $.ui.focusable( element, $.attr( element, "tabindex" ) !== null );
+		}
+	});
+
+//https://github.com/jquery/jquery-ui/blob/master/ui/escape-selector.js
+	$.ui.escapeSelector = ( function() {
+		var selectorEscape = /([!"#$%&'()*+,./:;<=>?@[\]^`{|}~])/g;
+		return function( selector ) {
+			return selector.replace( selectorEscape, "\\$1" );
+		};
+	})();
 };
